c4a84ee60a746a3fc5ad65ee40420ccdce926c5b
Zaheer <abdulzaheer963@gmail.com>
2025-08-25 19:25:08 +0530

    only 3

---
+++ Greatest_of_three.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int large3 = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.print(large3);
+    }
+}
