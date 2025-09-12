dedb9578712deb415b0ebacba442e9793d5656b0
Zaheer <abdulzaheer963@gmail.com>
2025-09-12 20:15:30 +0530

    return the word of maximum length

---
+++ Longest_word_in_a_sentence.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String s = sc.nextLine();
+        String word[]=s.split(" ");
+        String max="";
+        for(int i=0; i<word.length; i++)
+        {
+            if(word[i].length()>max.length())
+            {
+                max = word[i];
+            }
+        }
+        System.out.print(max);
+    }
+}
