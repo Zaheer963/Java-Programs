18bbbe6100f37e907f6569d162e1c82e616f6848
Zaheer <abdulzaheer963@gmail.com>
2025-09-23 20:00:56 +0530

    determine whether string2 is a rotation of string1

---
+++ Check_String_Rotation.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = s1+s1;
+        System.out.print(s3.contains(s2));
+    }
+}
